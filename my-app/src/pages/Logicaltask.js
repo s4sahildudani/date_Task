af4f80021300a85296b63db2ae420cdcc61eab8e
Sahil Dudani <s4sahil.dudani@gmail.com>
2023-05-10 16:11:42 +0530

    map method

--- my-app/src/pages/Logicaltask.js
+++ my-app/src/pages/Logicaltask.js
@@ -1,56 +1,17 @@
-
 import React, { useState } from "react";
 import { Typography, ListItemText, List, ListItem } from "@mui/material";
-
 function Logicaltask() {
-    const [data] = useState(['toy', 'shop', 'toy', 'cake', 'pastry', 'shop']);
-
-    const counts = {};
-  
-    for (let i = 0; i < data.length; i++) {
-      const item = data[i];
-      if (counts[item]) {
-        counts[item]++;
-      } else {
-        counts[item] = 1;
-      }
-    }
-  
-    const elements = [];
-  
-    // Display specific lines
-    if (counts['toy']) {
-      elements.push(
-        <ListItem key="toy">
-          <ListItemText primary={`toy = ${counts['toy']}`} />
-        </ListItem>
-      );
-    }
-  
-    if (counts['shop']) {
-      elements.push(
-        <ListItem key="shop">
-          <ListItemText primary={`shop = ${counts['shop']}`} />
-        </ListItem>
-      );
-    }
-  
-    if (counts['cake']) {
-      elements.push(
-        <ListItem key="cake">
-          <ListItemText primary={`cake = ${counts['cake']}`} />
-        </ListItem>
-      );
-    }
-  
-    if (counts['pastry']) {
-      elements.push(
-        <ListItem key="pastry">
-          <ListItemText primary={`pastry = ${counts['pastry']}`} />
-        </ListItem>
-      );
-    }
-
+  const [data] = useState(['toy', 'shop', 'toy', 'cake', 'pastry', 'shop']);
+  const counts = {};
+  for (let i = 0; i < data.length; i++) {
+    const item = data[i];
+    counts[item] = counts[item] ? counts[item] + 1 : 1;
+  }
+  const elements = Object.keys(counts).map((key) => (
+    <ListItem key={key}>
+      <ListItemText primary={`${key} = ${counts[key]}`} />
+    </ListItem>
+  ));
   return (
     <>
       <Typography variant="h6">Output</Typography>
@@ -58,5 +19,4 @@ function Logicaltask() {
     </>
   );
 }
-
 export default Logicaltask;
